911a90258ce911d7413598abc38e8d66bd0b7e5c
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-08-15 21:43:35 -0500

    Change module and move view files
    olo

--- workspace/clinicaVeterinaria/src/main/java/module-info.java
+++ workspace/clinicaVeterinaria/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module co.edu.uniquindio.clinicaVeterinaria {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires jpro.webapi;
 
-    opens co.edu.uniquindio.clinicaVeterinaria.view to javafx.fxml;
     exports co.edu.uniquindio.clinicaVeterinaria.application;
 }
