127c22e447368a160970dbc35f22edbee584654b
Tourment0412 <santiquinterouribe0412@gmail.com>
2023-08-20 23:26:15 -0500

    Terminado Esqueleto RegistroMascota

--- workspace/clinicaVeterinaria/src/main/java/co/edu/uniquindio/clinicaVeterinaria/controllers/RegistroMascotaController.java
+++ workspace/clinicaVeterinaria/src/main/java/co/edu/uniquindio/clinicaVeterinaria/controllers/RegistroMascotaController.java
@@ -1,5 +1,49 @@
 package co.edu.uniquindio.clinicaVeterinaria.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class RegistroMascotaController {
+
+    @FXML
+    private Label lblSexoMascota;
+
+    @FXML
+    private Button btnRegistrarMascota;
+
+    @FXML
+    private TextField txtNombreMascota;
+
+    @FXML
+    private Label lblRazaMascota;
+
+    @FXML
+    private TextField txtRazaMascota;
+
+    @FXML
+    private TextField txtSexoMascota;
+
+    @FXML
+    private Label lblNombreMascota;
+
+    @FXML
+    private TextField txtTipoMascota;
+
+    @FXML
+    private Label lblEdadMascota;
+
+    @FXML
+    private TextField txtEdadMascota;
+
+    @FXML
+    private Label lblTipoMascota;
+
+    @FXML
+    void registrarMascota(MouseEvent event) {
+
+    }
 
 }
