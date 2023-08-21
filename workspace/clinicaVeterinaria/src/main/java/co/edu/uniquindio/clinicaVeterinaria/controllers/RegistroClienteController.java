ca07c9b16d42eef1ed6a08b1f4d34a60e1cf12c9
Tourment0412 <santiquinterouribe0412@gmail.com>
2023-08-20 22:37:07 -0500

    Esqueleto RegistroCliente

---
+++ workspace/clinicaVeterinaria/src/main/java/co/edu/uniquindio/clinicaVeterinaria/controllers/RegistroClienteController.java
@@ -0,0 +1,53 @@
+package co.edu.uniquindio.clinicaVeterinaria.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextField;
+
+public class RegistroClienteController {
+
+
+    @FXML
+    private TextField txtCorreoCliente;
+
+    @FXML
+    private TextField txtTelefonoCliente;
+
+    @FXML
+    private Label lblCorreoCliente;
+
+    @FXML
+    private Label lblNombreCliente;
+
+    @FXML
+    private Button btnRegistrarCliente;
+
+    @FXML
+    private TextField txtNombreCliente;
+
+    @FXML
+    private Label lblCedulaCliente;
+
+    @FXML
+    private Label lblDireccionCliente;
+
+    @FXML
+    private TextField txtCedulaCliente;
+
+    @FXML
+    private TextField txtDireccionCliente;
+
+    @FXML
+    private Label lblTelefonoCliente;
+
+    @FXML
+    private Label lblTitleCliente;
+
+    @FXML
+    void registrarCliente(MouseEvent event) {
+
+    }
+
+}
