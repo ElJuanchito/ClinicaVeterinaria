338c39d42fc4fc4a5b42eafde6aafafa2434e1ad
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-08-22 22:30:11 -0500

    Implemented 2 dependencies
    Added a way to generate pdfs with javafx
    Added a way to navigate between routes

--- workspace/clinicaVeterinaria/src/main/java/module-info.java
+++ workspace/clinicaVeterinaria/src/main/java/module-info.java
@@ -1,11 +1,16 @@
 module co.edu.uniquindio.clinicaVeterinaria {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jpro.webapi;
-    requires javafx.graphics;
-    exports co.edu.uniquindio.clinicaVeterinaria.application;
-    exports co.edu.uniquindio.clinicaVeterinaria.services;
-    exports co.edu.uniquindio.clinicaVeterinaria.tests;
-    opens co.edu.uniquindio.clinicaVeterinaria.controllers to javafx.fxml;
-    exports co.edu.uniquindio.clinicaVeterinaria.controllers to javafx.fxml;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires jpro.webapi;
+	requires javafx.graphics;
+	requires one.jpro.routing.core;
+	requires org.apache.pdfbox;
+
+	exports co.edu.uniquindio.clinicaVeterinaria.application;
+	exports co.edu.uniquindio.clinicaVeterinaria.services;
+	exports co.edu.uniquindio.clinicaVeterinaria.tests;
+
+	opens co.edu.uniquindio.clinicaVeterinaria.controllers to javafx.fxml;
+
+	exports co.edu.uniquindio.clinicaVeterinaria.controllers to javafx.fxml;
 }
