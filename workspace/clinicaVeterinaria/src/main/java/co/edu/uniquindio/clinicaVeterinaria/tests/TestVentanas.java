a490b9cfd777036b2e77bb5de99c1f37a3acb352
ElJuanchito <perdomocardenas18@gmail.com>
2023-08-24 14:21:34 -0500

    Modificando el profile selector

--- workspace/clinicaVeterinaria/src/main/java/co/edu/uniquindio/clinicaVeterinaria/tests/TestVentanas.java
+++ workspace/clinicaVeterinaria/src/main/java/co/edu/uniquindio/clinicaVeterinaria/tests/TestVentanas.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.clinicaVeterinaria.tests;
 import java.io.IOException;
 
 import co.edu.uniquindio.clinicaVeterinaria.application.App;
+import co.edu.uniquindio.clinicaVeterinaria.controllers.ModelFactoryController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,13 +14,14 @@ public class TestVentanas extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ModelFactoryController.getInstance().saveData();
 		launch();
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		Scene scena = new Scene(loadFXML("FinalizarAtencion"));
+		Scene scena = new Scene(loadFXML("profileSelector"));
 		//scena.getStylesheets().add(getClass().getResource("/co/edu/uniquindio/clinicaVeterinaria/styles/textStyle.css").toExternalForm());
 		primaryStage.setScene(scena);
 		primaryStage.show();
