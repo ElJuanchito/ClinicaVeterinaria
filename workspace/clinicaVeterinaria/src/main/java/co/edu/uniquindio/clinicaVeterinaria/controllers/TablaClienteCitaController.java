08c0cba4f2ced1490e4183599f5a20877e72056b
ElJuanchito <perdomocardenas18@gmail.com>
2023-08-29 22:36:41 -0500

    Update TablaClienteCitaController.java

--- workspace/clinicaVeterinaria/src/main/java/co/edu/uniquindio/clinicaVeterinaria/controllers/TablaClienteCitaController.java
+++ workspace/clinicaVeterinaria/src/main/java/co/edu/uniquindio/clinicaVeterinaria/controllers/TablaClienteCitaController.java
@@ -1,14 +1,9 @@
 package co.edu.uniquindio.clinicaVeterinaria.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import co.edu.uniquindio.clinicaVeterinaria.exceptions.ClienteNoExistenteException;
 import co.edu.uniquindio.clinicaVeterinaria.model.Cliente;
-import co.edu.uniquindio.clinicaVeterinaria.model.Mascota;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -24,74 +19,80 @@ import javafx.scene.input.MouseEvent;
  */
 public class TablaClienteCitaController {
 
-    @FXML
-    private Button btnSiguiente;
+	@FXML
+	private Button btnSiguiente;
 
-    @FXML
-    private TableColumn<Cliente, String> colCorreo;
+	@FXML
+	private TableColumn<Cliente, String> colCorreo;
 
-    @FXML
-    private TableColumn<Cliente, String> colDireccion;
+	@FXML
+	private TableColumn<Cliente, String> colDireccion;
 
-    @FXML
-    private TableColumn<Cliente, String> colNombre;
+	@FXML
+	private TableColumn<Cliente, String> colNombre;
 
-    @FXML
-    private TableColumn<Cliente, String> colCedula;
+	@FXML
+	private TableColumn<Cliente, String> colCedula;
 
-    @FXML
-    private TableColumn<Cliente, String> colTelefono;
+	@FXML
+	private TableColumn<Cliente, String> colTelefono;
 
-    @FXML
-    private Label lblDueno;
+	@FXML
+	private Label lblDueno;
 
-    @FXML
-    private TableView<Cliente> tblCliente;
+	@FXML
+	private TableView<Cliente> tblCliente;
 
-    @FXML
-    private TextField txtCedula;
+	@FXML
+	private TextField txtCedula;
 
-	private ObservableList<Cliente> listaObservable;
-	
 	private Cliente cliente;
 
-    @FXML
-    void seleccionarEvent(MouseEvent event) {
-    	seleccionarAction();
-    }
+	@FXML
+	void seleccionarEvent(MouseEvent event) {
+		seleccionarAction();
+	}
 
 	private void seleccionarAction() {
 		cliente = tblCliente.getSelectionModel().getSelectedItem();
-    	
-    	if (cliente == null) return;
-    	
-    	lblDueno.setText(cliente.getNombre());
+
+		if (cliente == null)
+			return;
+
+		lblDueno.setText(cliente.getNombre());
 	}
 
-    @FXML
-    void siguienteEvent(ActionEvent event) {
-    	try {
+	@FXML
+	void siguienteEvent(ActionEvent event) {
+		siguienteAction();
+	}
+
+	private void siguienteAction() {
+		if (cliente == null) {
+			Menucontroller.getInstance().crearAlerta("Debe seleccionar un cliente");
+			return;
+		}
+		try {
 			ModelFactoryController.getInstance().setCliente(cliente.getCedula());
 		} catch (ClienteNoExistenteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Menucontroller.getInstance().crearAlerta("El cliente con cedula " + cliente.getCedula() + " no existe");
 		}
-    }
+	}
 
-    @FXML
-    void initialize() {
-    	ModelFactoryController.getInstance().loadData();
-    	txtCedula.textProperty().addListener((observable, oldValue, newValue) -> {
+	@FXML
+	void initialize() {
+		ModelFactoryController.getInstance().loadData();
+		txtCedula.textProperty().addListener((observable, oldValue, newValue) -> {
 			tblCliente.setItems(FXCollections
 					.observableArrayList(ModelFactoryController.getInstance().filtrarClienteCedu(newValue)));
 			tblCliente.refresh();
 		});
-    	colNombre.setCellValueFactory(e -> new ReadOnlyStringWrapper(e.getValue().getNombre()));
+		colNombre.setCellValueFactory(e -> new ReadOnlyStringWrapper(e.getValue().getNombre()));
 		colCorreo.setCellValueFactory(e -> new ReadOnlyStringWrapper(e.getValue().getCorreo()));
 		colTelefono.setCellValueFactory(e -> new ReadOnlyStringWrapper(e.getValue().getTelefono()));
 		colCedula.setCellValueFactory(e -> new ReadOnlyStringWrapper(e.getValue().getCedula()));
 		colDireccion.setCellValueFactory(e -> new ReadOnlyStringWrapper(e.getValue().getDireccion()));
-    }
+		tblCliente.refresh();
+	}
 
 }
-
